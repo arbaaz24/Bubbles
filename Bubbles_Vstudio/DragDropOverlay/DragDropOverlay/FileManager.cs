9b42c93ea51f354fc4c30c90ef7243c90171e8ff
Arbaaz <nasaarbaaz@gmail.com>
2025-08-19 01:26:03 -0500

    stable test version

--- Bubbles_Vstudio/DragDropOverlay/DragDropOverlay/FileManager.cs
+++ Bubbles_Vstudio/DragDropOverlay/DragDropOverlay/FileManager.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// =============================
+// File: MainWindow.xaml.cs
+// =============================
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
